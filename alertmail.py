72e1b5e2bcb4933039a97b2d324bf47503c0710d
Nicolas Flores <nicolas.floress@mail.udp.cl>
2018-01-03 20:42:15 -0300

    Update alertmail.py

--- alertmail.py
+++ alertmail.py
@@ -3,22 +3,22 @@ from email.mime.text import MIMEText
 import smtplib
 
 
-def sendd(obj,price):
-	from_addr = 'msgbot@outlook.com'
-	to_addr  = 'mail'
-	msg = "Es momento de comprar {}, ${}".format(obj,price)
+def sendd(msg,price,dif):
+	from_addr = 'automsgbot24@gmail.com'
+	to_addr  = 'nicolas.floress@mail.udp.cl'
+	msg = "{}, ${}, vario ${}".format(msg,price,dif)
  
 	mime_msg=MIMEText(msg,"plain") #indica que el texto sea plano
 	#mime_message = MIMEText(message, "plain", _charset="utf-8")
 
 	mime_msg["From"]=from_addr 
 	mime_msg["To"]=to_addr
-	mime_msg["Subject"]="Mensaje automatico, cambio de precio en knasta" #asunto
+	mime_msg["Subject"]="Mensaje automatico, cambio de precio en Ripley" #asunto
  
-	server = smtplib.SMTP("smtp.live.com")
+	server = smtplib.SMTP("smtp.gmail.com")
 	server.starttls()
-	server.login(from_addr,"pass")
+	server.login(from_addr,"1122334455_1")
 	server.sendmail(from_addr, to_addr, mime_msg.as_string())
 	server.quit()
-#sendd(122)
+#sendd("ps4",122)
 
